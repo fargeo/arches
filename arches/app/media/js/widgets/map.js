1b8b3a0aef17bbd3af46f13d65fa6754fa4d6145
Rob Gaston <rgaston@fargeo.com>
2016-08-03 13:06:00 -0700

    Changing MapboxGL Styles

--- arches/app/media/js/widgets/map.js
+++ arches/app/media/js/widgets/map.js
@@ -1,4 +1,10 @@
-define(['knockout', 'viewmodels/widget', 'bindings/mapboxgl'], function (ko, WidgetViewModel) {
+define([
+    'knockout',
+    'viewmodels/widget',
+    'map/mapbox-street-style',
+    'map/mapbox-satellite-style',
+    'bindings/mapboxgl'
+], function (ko, WidgetViewModel, mapStreetStyle, mapSatelliteStyle) {
     /**
     * registers a map-widget component for use in forms
     * @function external:"ko.components".map-widget
@@ -7,6 +13,20 @@ define(['knockout', 'viewmodels/widget', 'bindings/mapboxgl'], function (ko, Wid
     return ko.components.register('map-widget', {
         viewModel: function(params) {
             WidgetViewModel.apply(this, [params]);
+
+            this.mapOptions = {
+                style: mapSatelliteStyle
+            };
+
+            var self = this
+            this.setStyle = function(style) {
+                self.map.setStyle(style)
+            };
+
+            this.mapStreetStyle = mapStreetStyle;
+            this.mapSatelliteStyle = mapSatelliteStyle;
+
+            
         },
         template: { require: 'text!widget-templates/map' }
     });
