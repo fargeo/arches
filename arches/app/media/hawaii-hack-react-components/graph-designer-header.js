6ae1f6fb257e7c1ae7623d29d4cfb741618af8d2
Christopher Byrd <cbyrd@fargeo.com>
2022-04-12 15:13:41 -0700

    adds basic React demo

--- arches/app/media/hawaii-hack-react-components/graph-designer-header.js
+++ arches/app/media/hawaii-hack-react-components/graph-designer-header.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+
+function Welcome(props) {
+    const updatedLoveMessage = '( ' + props.loveMessage + ' )';
+
+    const [isTitleButtonClicked, setTitleButtonClickedState] = useState(false);
+
+    const titleStyle = {
+        backgroundColor: isTitleButtonClicked ? 'green' : 'pink',
+        fontSize: '28px',
+        fontWeight: 'bold',
+        marginRight: '10px',
+    };
+
+    return (
+        <div style={{display: 'flex', alignItems: 'center', width: '100%'}}>
+            <div style={titleStyle}>Hello, I'm a React header {updatedLoveMessage}</div>
+            <button onClick={() => setTitleButtonClickedState(!isTitleButtonClicked)}>CLICK ME</button>
+        </div>
+    );
+}
 
 const root = createRoot(
     document.getElementById('hawaii-hackathon-header-root')
 );
-
-root.render(<h1>Hello, I'm a React header ( and I love you )</h1>)
+root.render(<Welcome loveMessage='and I love you' />)
