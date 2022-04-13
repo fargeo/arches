e6da5ceedf6bb4acafe91be2ecf388050b2c2777
Christopher Byrd <cbyrd@fargeo.com>
2022-04-13 11:26:16 -0700

    nit

--- arches/app/media/hawaii-hack-react-components/react-header.js
+++ arches/app/media/hawaii-hack-react-components/react-header.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('hawaii-hackathon-header-root');
-const root = createRoot(container);
 
 function Header(props) {
     const updatedMessage = '( ' + props.message + ' )';
@@ -10,18 +9,18 @@ function Header(props) {
     const [isTitleButtonClicked, setTitleButtonClickedState] = useState(false);
 
     const titleStyle = {
-        backgroundColor: isTitleButtonClicked ? 'green' : 'pink',
         fontSize: '28px',
         fontWeight: 'bold',
         marginRight: '10px',
     };
 
     return (
-        <div style={{display: 'flex', alignItems: 'center', width: '100%'}}>
+        <div style={{display: 'flex', alignItems: 'center', width: '100%', backgroundColor: isTitleButtonClicked ? 'green' : 'pink',}}>
             <div style={titleStyle}>Hello, I'm a React header {updatedMessage}</div>
-            <button onClick={() => setTitleButtonClickedState(!isTitleButtonClicked)}>CLICK ME!</button>
+            <button class='btn btn-small btn-info' onClick={() => setTitleButtonClickedState(!isTitleButtonClicked)}>CLICK ME!</button>
         </div>
     );
 }
 
+const root = createRoot(container);
 root.render(<Header message={container.getAttribute('customMessage')} />)
